eeb51373ebe400d60d14bb7df068f4a68f60341a
phuongwj <phamjulia9@gmail.com>
2024-12-22 18:47:36 -0400

    Created simple Header for my name and stuffs

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <section>
+        <div>
+            Hi, I'm
+        </div>
+        <div>
+            Phuong (aka Julia)
+        </div>
+        <p>
+            - I'm an aspiring &
+        </p>
+        <p>
+            hardworking developer
+        </p>
+    </section>
+    
+  )
+}
