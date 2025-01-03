92b4e04a217726fb9d0ebaf3d5ab6670da5fa65a
phuongwj <phamjulia9@gmail.com>
2025-01-03 14:12:58 -0400

    Added okay-ish styling for the box and made the box smaller as well

--- src/app/components/Experiences.jsx
+++ src/app/components/Experiences.jsx
@@ -5,22 +5,22 @@ export default function Experiences() {
     return (
         <div className="relative group">
             <div
-                className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-full blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200">
+                className="absolute h-full w-full bg-gradient-to-b from-white rounded-3xl blur opacity-25 group-hover:opacity-70 transition duration-1000 group-hover:duration-500">
             </div>
-            <main className="relative flex flex-col gap-3 p-8 rounded-3xl shadow-xl bg-secondary">
-                <section className="font-bold text-2xl pb-1 text-white opacity-70">
+            <main className="relative flex flex-col gap-3 p-6 rounded-3xl shadow-xl bg-secondary border border-secondaryC">
+                <section className="font-bold text-xl pb-1 text-white opacity-70">
                     Experiences
                 </section>
-                <section className="h-44 px-4 overflow-y-scroll">
+                <section className="h-36 px-2 overflow-y-scroll">
                     <ul className="max-h-full flex flex-col gap-4">
                         {exps.reverse().map( (exp) => (
                             <li key={exp.id} className="flex flex-col">
-                                <section className="text-lg flex flex-row justify-between gap-32">
+                                <section className="text-base flex flex-row justify-between gap-32">
                                     <h2 className="text-white font-semibold opacity-45">{exp.title}</h2>
                                     <p className="text-secondaryC font-medium">{exp.time}</p>
                                 </section>
-                                <h3 className="text-base text-secondaryC">{exp.org}</h3>
-                                <h4 className="text-base text-secondaryC">{exp.location}</h4>
+                                <h3 className="text-sm text-secondaryC">{exp.org}</h3>
+                                <h4 className="text-sm text-secondaryC">{exp.location}</h4>
                             </li>
                         ))}
                     </ul>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,7 @@ export default {
         secondary: '#191919',
         mainC: '#467CCC',
         secondaryC: '#4D4D4D',
+        wless: '#838383',
       },
       height: {
         '6.5': '1.625rem',
