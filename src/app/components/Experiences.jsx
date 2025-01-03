7fd534916bc91e4db7adfb325ca9bf184bafd213
phuongwj <phamjulia9@gmail.com>
2025-01-03 16:35:41 -0400

    Layout-ish for Experiences.jsx updated

--- src/app/components/Experiences.jsx
+++ src/app/components/Experiences.jsx
@@ -11,16 +11,34 @@ export default function Experiences() {
                 <section className="font-bold text-xl pb-1 text-white opacity-70">
                     Experiences
                 </section>
-                <section className="h-36 px-2 overflow-y-scroll">
+                <section className="h-28 px-4 overflow-y-scroll">
                     <ul className="max-h-full flex flex-col gap-4">
                         {exps.reverse().map( (exp) => (
-                            <li key={exp.id} className="flex flex-col">
+                            <li key={exp.id} className="relative flex flex-col">
+                                {/* Invisible checkbox */}
+                                <input 
+                                    className="absolute peer w-full h-6 z-10 opacity-0" 
+                                    type="checkbox"
+                                />
                                 <section className="text-base flex flex-row justify-between gap-32">
-                                    <h2 className="text-white font-semibold opacity-45">{exp.title}</h2>
+                                    {/* Arrow Icon */}
+                                    <section className="flex flex-row">
+                                        <div className="opacity-0">hing</div>
+                                        <h2 className="text-white font-semibold opacity-45">{exp.title}</h2>
+                                    </section>
                                     <p className="text-secondaryC font-medium">{exp.time}</p>
                                 </section>
-                                <h3 className="text-sm text-secondaryC">{exp.org}</h3>
-                                <h4 className="text-sm text-secondaryC">{exp.location}</h4>
+                                <section className="absolute rotate-0 peer-checked:rotate-180">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#4D4D4D" 
+                                                className="size-6 rotate-0 transition-transform duration-500 peer-checked:rotate-180">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </section>
+                                {/* Content */}
+                                <section className="overflow-hidden transition-all duration-500 bg-white max-h-0 peer-checked:max-h-40">
+                                    <h3 className="text-base text-secondaryC">{exp.org}</h3>
+                                    <h4 className="text-base text-secondaryC">{exp.location}</h4>
+                                </section>
                             </li>
                         ))}
                     </ul>
