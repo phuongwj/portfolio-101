e6a4868f7ba15826cd5588f4a157ade794a95e17
phuongwj <phamjulia9@gmail.com>
2025-01-03 20:19:04 -0400

    Another idea for Experiences.jsx?

--- src/app/components/Experiences.jsx
+++ src/app/components/Experiences.jsx
@@ -9,15 +9,15 @@ export default function Experiences() {
                 className="absolute h-full w-full bg-gradient-to-b from-white rounded-3xl blur opacity-25 group-hover/bgg:opacity-70 transition duration-700"
             >
             </section>
-            <main className="relative flex flex-col gap-3 p-7 rounded-3xl shadow-xl bg-secondary border border-secondaryC">
+            <main className="relative flex flex-col gap-3 p-7 rounded-3xl shadow-xl bg-secondary border border-secondaryC transition duration-500 opacity-90 group-hover/bgg:opacity-100">
                 <section className="flex flex-row gap-2 pb-1">
                     <section className="pt-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="size-6 fill-white/45 group-hover/bgg:fill-white/80 transition duration-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="size-6 fill-white/80">
                             <path fillRule="evenodd" 
                             d="M4.5 9.75a6 6 0 0 1 11.573-2.226 3.75 3.75 0 0 1 4.133 4.303A4.5 4.5 0 0 1 18 20.25H6.75a5.25 5.25 0 0 1-2.23-10.004 6.072 6.072 0 0 1-.02-.496Z" clip-rule="evenodd" />
                         </svg>
                     </section>
-                    <p className="font-bold text-xl text-white/45 group-hover/bgg:text-white/80 transition duration-500"> Experiences </p>
+                    <p className="font-bold text-xl text-white/80"> Experiences </p>
                 </section>  
                 <section className="h-30 px-4 overflow-y-scroll">
                     <ul className="max-h-full flex flex-col gap-4">
@@ -29,8 +29,8 @@ export default function Experiences() {
                                     type="checkbox"
                                 />
                                 <section className="text-base flex flex-row pl-8 justify-between gap-32">
-                                    <h2 className="group-hover/list:text-mainC/100 group-hover/bgg:text-white/65 text-white/30 transition duration-500 font-semibold">{exp.title}</h2>
-                                    <p className="group-hover/list:text-white/50 group-hover/bgg:text-white/30 text-white/15 transition duration-500 font-medium">{exp.time}</p>
+                                    <h2 className="group-hover/list:text-mainC/100 text-white/65 transition duration-500 font-semibold">{exp.title}</h2>
+                                    <p className="group-hover/list:text-white/50 text-white/30 transition duration-500 font-medium">{exp.time}</p>
                                 </section>
                                 {/* Arrow Icon */}
                                 <section className="absolute rotate-0 peer-checked:rotate-90 transition-all duration-500">
