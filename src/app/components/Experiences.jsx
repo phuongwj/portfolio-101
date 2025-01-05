c3508bf8a2b6d0b8d47e9927f3e04793600b4369
phuongwj <phamjulia9@gmail.com>
2025-01-05 00:13:43 -0400

    Made the exps array be sorted before rendering

--- src/app/components/Experiences.jsx
+++ src/app/components/Experiences.jsx
@@ -3,6 +3,8 @@ import exps from "@/local-json/experiences";
 
 export default function Experiences() {
 
+    const expsSorted = exps.sort((a, b) => b.id - a.id);
+
     return (
         <section className="relative group/bgg">
             <section
@@ -21,8 +23,8 @@ export default function Experiences() {
                     <p className="font-bold text-xl text-white/45 transition duration-500 group-hover/bgg:text-white/80"> Experiences </p>
                 </section>  
                 <section className="h-30 px-4 overflow-y-scroll">
-                    <ul className="max-h-full flex flex-col-reverse gap-4">
-                        {exps.map( (exp) => (
+                    <ul className="max-h-full flex flex-col gap-4">
+                        {expsSorted.map((exp) => (
                             <li key={exp.id} className="relative group/list flex flex-col transition duration-500 hover:bg-huh/30">
                                 {/* Invisible checkbox */}
                                 <input 
