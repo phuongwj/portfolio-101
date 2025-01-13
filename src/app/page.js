ebff4293dfe6dd66b42e72170b60fbce04c92d58
phuongwj <phamjulia9@gmail.com>
2025-01-13 18:56:10 -0400

    Updated stuffs for winter

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react'
-import { Carousel } from "@material-tailwind/react";
+import { Carousel, IconButton } from "@material-tailwind/react";
 import Header from "@/app/components/Header";
 import Experiences from "@/app/components/Experiences";
 import Projects from "@/app/components/Projects";
@@ -10,12 +10,10 @@ export default function page() {
   return (
     <main className="flex flex-col items-center">
           <Header/>
-          <section className="max-w-[690px] h-screen">
-            <Carousel>
+          <section>
               <Experiences/>
-              <Projects/>
-              <Toolbox/>
-            </Carousel> 
+              {/* <Projects/>
+              <Toolbox/> */}
           </section>
     </main>
   )
