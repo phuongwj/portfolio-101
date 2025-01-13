e5a8c4ea13a49cdb8ab7fcd99d21234c696075bc
phuongwj <phamjulia9@gmail.com>
2025-01-13 00:35:25 -0400

    Added a side branch to work on another idea of the website, with the purpose to make the website look more minimalistic!

--- src/app/components/Experiences.jsx
+++ src/app/components/Experiences.jsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import exps from "@/local-json/experiences";
 import Link from 'next/link';
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
+'use client';
 import React from 'react'
+import { Carousel } from "@material-tailwind/react";
+import Header from "@/app/components/Header";
+import Experiences from "@/app/components/Experiences";
+import Projects from "@/app/components/Projects";
+import Toolbox from "@/app/components/Toolbox";
 
 export default function page() {
   return (
-    <></>
+    <main className="flex flex-col items-center">
+          <Header/>
+          <section className="max-w-[690px] h-screen">
+            <Carousel>
+              <Experiences/>
+              <Projects/>
+              <Toolbox/>
+            </Carousel> 
+          </section>
+    </main>
   )
 }
+
+//flex flex-col py-12 gap-8 max-w-[690px]
