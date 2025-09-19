3f88f895f5f7fb888004c00d0abe4c6c961351b1
phuongwj <phamjulia9@gmail.com>
2025-09-19 14:58:13 -0300

    updated experiences part, dates are to be added

--- src/app/components/Experiences.jsx
+++ src/app/components/Experiences.jsx
@@ -4,16 +4,8 @@ import exps from "@/local-json/experiences";
 
 
 export default function Experiences() {
-
-    const [expandedId, setExpandedId] = useState(null);
       
     const expsSorted = exps.sort((a, b) => b.id - a.id);
-      
-    const handleClick = (id) => {
-        setExpandedId(expandedId === id ? null : id);
-    } 
-    
-    const [contentIsHovered, setContentHovered] = useState(false);
 
   return (
     <>
@@ -21,62 +13,33 @@ export default function Experiences() {
         <section className="w-full flex flex-col gap-4">
 
           {/* Experience Title */}
-          <Link href="/experiences"
-            className="w-fit inline-flex items-center gap-1 text-first fill-first text-lg font-medium hover:underline hover:underline-offset-4 hover:opacity-60 ease-in-out transition duration-300">
+          <div
+            className="w-fit text-first text-xl font-medium">
             <span>experience</span>
-            <span className="pt-0.5">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                  className="h-3.5 w-3.5">
-                <path d="M 3 3 L 3 21 L 21 21 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 3 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"/>
-              </svg>
-            </span>
-          </Link>
+          </div>
           
           {/* List of Experiences */}
           <section className="flex justify-center">
-              <ul className="flex flex-col gap-2">
+              <ul className="flex flex-col gap-2 w-full">
                   {expsSorted.slice(0, 4).map((exp) => (
-                      <li key={exp.id} className="relative group/list flex flex-col transition duration-500">
+                      <li key={exp.id} className="flex flex-col">
 
-                          {/* Invisible checkbox */}
-                          <input 
-                              className="absolute peer w-full h-12 z-10 opacity-0 cursor-pointer" 
-                              type="checkbox"
-                              checked={expandedId === exp.id}
-                              onChange={() => handleClick(exp.id)}
-                          />
+                        {/* Company Logo & the Position, Company */}
+                        <section className="text-md py-2 px-3 rounded-xl flex flex-row bg-widBg">
+                          <div className="flex items-center gap-3">
 
-                          {/* Position & Place */}
-                          <section className={contentIsHovered 
-                            ? "text-md p-3 pl-9 rounded-lg flex flex-row justify-between bg-widBg transition duration-500"
-                            : "text-md p-3 pl-9 rounded-lg flex flex-row justify-between bg-widBg group-hover/list:bg-first transition duration-500"}>
-                              <h2 className={contentIsHovered 
-                              ? "text-first transition duration-500 font-medium"
-                              : "text-first group-hover/list:text-widBg transition duration-500 font-medium"}>{exp.title} {exp.org} </h2>
-                              <p className="text-second transition duration-500 font-medium">{exp.time}</p>
-                          </section>
+                            <img
+                              src={exp.image}
+                              alt={`${exp.org} logo`}
+                              className="w-11 h-11 object-contain rounded-lg bg-white"
+                            />
 
-                          {/* Arrow Icon */} 
-                          <section className="absolute p-3.5 rotate-0 peer-checked:rotate-90 transition-all duration-500">
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" 
-                                className={contentIsHovered
-                                ? "size-5 fill-first transition duration-500"
-                                : "size-5 fill-first group-hover/list:fill-widBg transition duration-500"}>
-                                  <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" 
-                                      clipRule="evenodd" />
-                              </svg>
-                          </section>
-
-                          {/* Content */}
-                          <section 
-                            className={`mt-2 rounded-lg overflow-hidden bg-widBg/50 transition-all duration-500
-                            ${expandedId === exp.id ? 'max-h-40 opacity-100 px-8 py-3' : 'max-h-0 opacity-0 py-0'}`}
-                            onMouseEnter={() => setContentHovered(true)}
-                            onMouseLeave={() => setContentHovered(false)}
-                          >
-                              <h3 className="text-second transition duration-500 mb-0.5">- {exp.location}</h3>
-                              <h4 className="text-second transition duration-500">{exp.info} {exp.id === 2 ? <span> Check out the website </span>: ""} <span> {exp.id === 2 ? <Link href="https://hci4good.cs.dal.ca/" className="font-semibold underline" target="_blank"> here</Link> : ""} </span> </h4>
-                          </section>
+                            <div className="flex flex-col">
+                              <h2 className="text-first font-medium">{exp.title}</h2>
+                              <h3 className="text-second text-sm">{exp.org}</h3>
+                            </div>
+                          </div>
+                        </section>
                       </li>
                   ))}
               </ul>
